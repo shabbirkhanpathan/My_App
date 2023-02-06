3724f533ed41591790873a64f99b0b2b87ff5032
Shabbir Pathan <shabbirkhanpathan.soboft@gmail.com>
2023-02-06 18:41:03 +0530

    Add all files and folders to the branch

---
+++ RestOfYourApp.tsx
@@ -0,0 +1,17 @@
+/* eslint-disable prettier/prettier */
+import React, {useCallback} from 'react';
+import {useToast} from 'react-native-toast-notifications';
+import { Text, TouchableOpacity } from 'react-native';
+
+export const RestOfYourApp = () => {
+  const toast = useToast();
+  const handle = useCallback(() => {
+    toast.show('hey');
+  }, [toast]);
+
+  return (
+    <TouchableOpacity onPress={handle} style={{marginTop: 100}}>
+      <Text>Hey</Text>
+    </TouchableOpacity>
+  );
+};
