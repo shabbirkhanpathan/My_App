3e7d89f0b8b15ffbc68fb6468fff9396bf193b24
Shabbir Pathan <shabbirkhanpathan.soboft@gmail.com>
2023-02-13 20:51:40 +0530

    map add

--- App.tsx
+++ App.tsx
@@ -6,13 +6,31 @@
  */
 
 import React from 'react';
-import {ToastProvider} from 'react-native-toast-notifications';
-import {RestOfYourApp} from './RestOfYourApp';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {StyleSheet, Text, View} from 'react-native';
 
-export default function App() {
-  return (
-    <ToastProvider>
-      <RestOfYourApp />
-    </ToastProvider>
-  );
-}
+export default () => (
+  <View style={styles.container}>
+    <MapView
+      style={styles.map}
+      provider={PROVIDER_GOOGLE}
+      region={{
+        latitude: 37.78825,
+        longitude: -122.4324,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421,
+      }}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    // height: '100%',
+    // width: '100%',
+    flex: 1,
+  },
+});
